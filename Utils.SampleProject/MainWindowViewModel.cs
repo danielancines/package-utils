4bf24d0c61a8f20d3d6b5d5f5b113f9a1cd5edd8
Daniel Ancines <daniel.ancines@gmail.com>
2025-09-06 08:35:06 -0300

    .net upgrade

---
+++ Utils.SampleProject/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Utils.Commands;
+
+namespace Utils.SampleProject;
+
+public sealed class MainWindowViewModel
+{
+    public MainWindowViewModel()
+    {
+        this.ClickMeCommand = new ObservableCommand(parameter => System.Windows.MessageBox.Show("Hello from Click Me Command!"));
+    }
+    public ObservableCommand ClickMeCommand { get; }
+}
